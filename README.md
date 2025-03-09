3e2ae99ef5791b288226e74e221859870388ff57
dylanzhou415@gmail.com <dylanzhou415@gmail.com>
2025-03-09 12:44:38 +0100

    feat: add the processing data module

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# multimodal-vision-language-model
+# Multimodal Vision Language Model
 This Project is focous on the vision language model  using python and pytorch
--- processing_paligemma.py
+++ processing_paligemma.py
@@ -6,6 +6,71 @@ import torch
 IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5]
 IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
 
+def add_image_tokens_to_prompt(prefix_prompt, bos_token, image_seq_len, image_token):
+    # Quoting from the blog (https://huggingface.co/blog/paligemma#detailed-inference-process):
+    #   The input text is tokenized normally.
+    #   A <bos> token is added at the beginning, and an additional newline token (\n) is appended.
+    #   This newline token is an essential part of the input prompt the model was trained with, so adding it explicitly ensures it's always there.
+    #   The tokenized text is also prefixed with a fixed number of <image> tokens.
+    # NOTE: from the paper it looks like the `\n` should be tokenized separately, but in the HF implementation this is not done.
+    #       ref to HF implementation: https://github.com/huggingface/transformers/blob/7f79a97399bb52aad8460e1da2f36577d5dccfed/src/transformers/models/paligemma/processing_paligemma.py#L55-L73
+    return f"{image_token * image_seq_len}{bos_token}{prefix_prompt}\n"
+
+
+def resize(
+    image: Image,
+    size: Tuple[int, int],
+    resample: Image.Resampling = None,
+    reducing_gap: Optional[int] = None,
+) -> np.ndarray:
+    height, width = size
+    resized_image = image.resize(
+        (width, height), resample=resample, reducing_gap=reducing_gap
+    )
+    return resized_image
+
+""" rescale the pixel values from the range [0, 255] to [0, 1] rescale_factor=1 / 255.0 """
+def rescale(
+    image: np.ndarray, scale: float, dtype: np.dtype = np.float32
+) -> np.ndarray:
+    rescaled_image = image * scale
+    rescaled_image = rescaled_image.astype(dtype)
+    return rescaled_image
+
+""" Normalize the image: each of these value to be distributed around 0 with a standard deviation of 1"""
+def normalize(
+    image: np.ndarray,
+    mean: Union[float, Iterable[float]],
+    std: Union[float, Iterable[float]],
+) -> np.ndarray:
+    mean = np.array(mean, dtype=image.dtype)
+    std = np.array(std, dtype=image.dtype)
+    image = (image - mean) / std
+    return image
+
+def process_images(
+    images: List[Image.Image],
+    size: Dict[str, int] = None,
+    resample: Image.Resampling = None,
+    rescale_factor: float = None,
+    image_mean: Optional[Union[float, List[float]]] = None,
+    image_std: Optional[Union[float, List[float]]] = None,
+) -> List[np.ndarray]:
+    height, width = size[0], size[1]
+    images = [
+        resize(image=image, size=(height, width), resample=resample) for image in images
+    ]
+    # Convert each image to a numpy array
+    images = [np.array(image) for image in images]
+    # Rescale the pixel values to be in the range [0, 1]
+    images = [rescale(image, scale=rescale_factor) for image in images]
+    # Normalize the images to have mean 0 and standard deviation 1
+    images = [normalize(image, mean=image_mean, std=image_std) for image in images]
+    # Move the channel dimension to the first dimension. The model expects images in the format [Channel, Height, Width]
+    images = [image.transpose(2, 0, 1) for image in images]
+    return images
+
+
 class PaliGemmaProcessor:
 
     IMAGE_TOKEN = "<image>"
@@ -33,13 +98,78 @@ class PaliGemmaProcessor:
 
         self.tokenizer = tokenizer
     
+    """  How to use the PaliGemmaProcessor
+    Given a text prompt and an image, it will load the image pre-process it
+    resize it rescale it and normalize it. It will then tokenize the text prompt
+    """
+    
+    """ 
+    Why we create a __call__ funtion?
+    
+    This allows the instance of the processor to be called like a function.
+    
+    e.g. processor = PaliGemmaProcessor()
+         processer(...)
+         
+         Hier we just use one image and one text prompt
+    """ 
+    
     def __call__(
         self,
         text: List[str],
-        images: List[Image, Image],
+        images: List[Image.Image],
         padding: str = "longest",
         truncation: bool = True,
     ) -> dict:
         assert len(images) == 1 and len(text) == 1, f"Received {len(images)} images for {len(text)} prompts."
         
+        # Create a list of tensors 
+        pixel_values = process_images(
+            images,
+            size=(self.image_size, self.image_size),
+            resample=Image.Resampling.BICUBIC,
+            rescale_factor=1 / 255.0,
+            image_mean=IMAGENET_STANDARD_MEAN,
+            image_std=IMAGENET_STANDARD_STD,
+        )
         
+        # Convert thel list of numpy arrays to a single numpy array with the shape [Batch, Channels, Height, Width]
+        pixel_values = np.stack(pixel_values, axis=0)
+        # Convert the numpy array to a Pytorch tensor
+        pixel_values = torch.tensor(pixel_values)
+        
+        """ Crate the input in this Model: 
+            Create the tokens of the text prompt 
+            and create the placeholder for the image tokens
+        """
+        # Prepend a `self.image_seq_length` number of image tokens to the prompt
+        input_strings = [
+            add_image_tokens_to_prompt(
+                prefix_prompt=prompt,
+                bos_token=self.tokenizer.bos_token,
+                image_seq_len=self.image_seq_length,
+                image_token=self.IMAGE_TOKEN,
+            )
+            for prompt in text
+        ]
+        
+        """  Returns the input_ids and attention_mask as PyTorch tensors
+        [HELLO WORLD] 
+        -> tokenized-> [5, 2, 9] 
+        -> embedding -> [[1, ..., 1024], [1, ..., 1024], [1, ..., 1024]] """
+        inputs = self.tokenizer(
+            input_strings,
+            return_tensors="pt",
+            padding=padding,
+            truncation=truncation,
+        )
+
+        """ resize the image converted into a tensor 
+            -> take the prompt that we have added the som placeholder for the image tokens
+            -> then we add the prompt of the user 
+            -> and then the slash and character as indiated by paligemma"""
+        return_data = {"pixel_values": pixel_values, **inputs}
+        
+        # our processor will return the stuff that we need to pass to the model
+        
+        return return_data
